e6c6fdf1847105758a473124733933a1440f1e89
Amit Pile <1032202075@mitwpu.edu.in>
2023-06-04 14:32:42 +0530

    Final changes

--- todo_app_main/models.py
+++ todo_app_main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from django.db import DEFAULT_DB_ALIAS
 
 def one_week_hence():
     return timezone.now() + timezone.timedelta(days=7)
@@ -10,6 +11,15 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        #check if tag exists in the database
+        existing_tag = Tag.objects.using(DEFAULT_DB_ALIAS).filter(name__iexact=self.name).first()
+        if existing_tag:
+            self.id = existing_tag.id
+        else:
+            self.name = self.name.lower()
+            super().save(*args, **kwargs)
     
 class ToDoItem(models.Model):
 
@@ -21,10 +31,10 @@ class ToDoItem(models.Model):
     )
     title = models.CharField(max_length=100)
     timestamp = models.DateTimeField(auto_now_add=True)
-    description = models.TextField(null=True, blank=True)   
+    description = models.TextField()   
     due_date = models.DateTimeField(default=one_week_hence)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='OPEN')
-    tags = models.ManyToManyField('Tag', blank=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return f"{self.title}: due {self.due_date}"
--- todo_app_main/serializers.py
+++ todo_app_main/serializers.py
@@ -13,14 +13,30 @@ class TodoSerializer(serializers.ModelSerializer):
         model = ToDoItem
         fields = '__all__'
 
+    # def create(self, validated_data):
+    #     tags_data = validated_data.pop('tags')
+    #     todo = ToDoItem.objects.create(**validated_data)
+    #     #check if tag exists, if not create it and if it does, get it
+    #     for tag_data in tags_data:
+    #         #if the tag exists , without creating duplicate tags add the existing tag to the todo without any tag already exists error
+    #         tag, created = Tag.objects.get_or_create(name=tag_data['name'])
+    #         todo.tags.add(tag)
+    #     return todo
+    
     def create(self, validated_data):
         tags_data = validated_data.pop('tags')
         todo = ToDoItem.objects.create(**validated_data)
-        #check if tag exists, if not create it and if it does, get it
+        
+        existing_tags = {}
         for tag_data in tags_data:
-            #if the tag exists , without creating duplicate tags add the existing tag to the todo without any tag already exists error
-            tag, created = Tag.objects.get_or_create(name=tag_data['name'])
+            tag_name = tag_data['name']
+            if tag_name in existing_tags:
+                tag = existing_tags[tag_name]
+            else:
+                tag, created = Tag.objects.get_or_create(name=tag_name)
+                existing_tags[tag_name] = tag
             todo.tags.add(tag)
+            
         return todo
     
     def update(self, instance, validated_data):
